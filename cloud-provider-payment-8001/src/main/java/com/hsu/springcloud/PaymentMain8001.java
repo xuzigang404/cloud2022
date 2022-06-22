6be79ffd780f618785940cc793a604843a7b02e9
Xu <1284536511@qq.com>
2022-06-22 22:27:45 +0800

    新建项目

---
+++ cloud-provider-payment-8001/src/main/java/com/hsu/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.hsu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@Description 支付模块主启动类
+ *@Param
+ *@return
+ *@Author xzg
+ *@Date 2022/6/21
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
